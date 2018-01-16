d203ede3f7a8ecfa3cdd0ada12ec95feeb2046d8
illisonmatos <illisonmatos@hotmail.com>
2018-01-16 08:44:36 -0300

    Initial commit

---
+++ app/src/main/java/ticonsultoria/treinos/aPrincipal.java
@@ -0,0 +1,13 @@
+package ticonsultoria.treinos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_principal);
+    }
+}
